d1b2b8fafbb59fa5b48e96f30a8af1448e65aed8
Alexis-Dr <alexeydruzik@inbox.ru>
2019-12-25 23:42:38 +0300

    Fix.

--- src/by/bntu/fitr/povt/alexeyd/lectureSerializable/example1/Student.java
+++ src/by/bntu/fitr/povt/alexeyd/lectureSerializable/example1/Student.java
@@ -2,7 +2,7 @@ package by.bntu.fitr.povt.alexeyd.lectureSerializable.example1;
 
 import java.io.Serializable;
 
-public class Student implements Serializable {
+public class Student {
 
     private String name;
     private int age;
--- src/by/bntu/fitr/povt/alexeyd/lectureSerializable/example1/Test.java
+++ src/by/bntu/fitr/povt/alexeyd/lectureSerializable/example1/Test.java
@@ -2,16 +2,18 @@ package by.bntu.fitr.povt.alexeyd.lectureSerializable.example1;
 
 public class Test {
 
+    private static final String SRC_RESOURCES_INPUT_BINARY_PATH_UNIX = "src/resources/student.bin";
+    private static final String SRC_RESOURCES_INPUT_BINARY_PATH_WINDOWS = "c:\\Test\\student.bin";
+
     public static void main(String[] args) {
-        String fileName = "c:\\Test\\student.bin";
 
         Student student = new Student("Alex", 18, 7.8);
 
-        FileWorkerAfterJDK7.write(fileName, student);
+        FileWorkerAfterJDK7.write(SRC_RESOURCES_INPUT_BINARY_PATH_UNIX, student);
 
         System.out.println("Before: " + student);
 
-        Student otherStudent = FileWorkerAfterJDK7.read(fileName);
+        Student otherStudent = FileWorkerAfterJDK7.read(SRC_RESOURCES_INPUT_BINARY_PATH_UNIX);
 
         System.out.println("After: " + otherStudent);
     }
