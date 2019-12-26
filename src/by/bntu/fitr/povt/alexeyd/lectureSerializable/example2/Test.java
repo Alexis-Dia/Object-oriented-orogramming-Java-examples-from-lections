cbce7e76f1aecb6408a30606870c4e2ef566f8c3
Alexis-Dr <alexeydruzik@inbox.ru>
2019-12-26 22:24:48 +0300

    Fix.

--- src/by/bntu/fitr/povt/alexeyd/lectureSerializable/example2/Test.java
+++ src/by/bntu/fitr/povt/alexeyd/lectureSerializable/example2/Test.java
@@ -2,6 +2,9 @@ package by.bntu.fitr.povt.alexeyd.lectureSerializable.example2;
 
 public class Test {
 
+    private static final String SRC_RESOURCES_INPUT_BINARY_PATH_UNIX = "src/resources/student.bin";
+    private static final String SRC_RESOURCES_INPUT_BINARY_PATH_WINDOWS = "c:\\Test\\student.bin";
+
     public static void main(String[] args) {
         String fileName = "c:\\Test\\student.bin";
 
@@ -9,9 +12,9 @@ public class Test {
 
         System.out.println("Before: " + student);
 
-        FileWorkerSinceJDK7WithAutomaticSerialization.write(fileName, student);
+        FileWorkerSinceJDK7WithAutomaticSerialization.write(SRC_RESOURCES_INPUT_BINARY_PATH_UNIX, student);
 
-        Student otherStudent = FileWorkerSinceJDK7WithAutomaticSerialization.read(fileName);
+        Student otherStudent = FileWorkerSinceJDK7WithAutomaticSerialization.read(SRC_RESOURCES_INPUT_BINARY_PATH_UNIX);
 
         System.out.println("After: " + otherStudent);
     }
